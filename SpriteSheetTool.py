e0c8c3fa3767f85c1d0a53ff3a66dc41b26372a7
unknown <brodyclark@gmail.com>
2024-01-13 18:33:35 -0800

    can successfully draw with set_pixel. Need to add dialog to plugin to set col/row

--- SpriteSheetTool.py
+++ SpriteSheetTool.py
@@ -24,12 +24,13 @@ def Combine_Layers_To_Sheet():
         cols = layer_count / 2 
     else:
         cols = (layer_count + 1) / 2 
+    cols -= 1
 
     final_height = cols * img_height
     final_width = cols * img_width
 
     # add new layer to draw on
-    combined_layer = gimp.Layer(img,"Sprite Sheet",final_width,final_height,RGB_IMAGE,100,NORMAL_MODE)
+    combined_layer = gimp.Layer(img,"Sprite Sheet",final_width,final_height,RGBA_IMAGE,100,NORMAL_MODE)
     img.add_layer(combined_layer,0)
 
     col_count = 0
@@ -38,32 +39,24 @@ def Combine_Layers_To_Sheet():
 
     # draw original pixel region in new layer
     for layer in layers:
-
-        combined_layer.flush()
-        combined_layer.merge_shadow(True)
         combined_layer.update(0, 0, final_width, final_height)
-
+    
         source_width = layer.width
         source_height = layer.height
-        source_region = layer.get_pixel_rgn(0, 0, source_width, source_height, False, False)
-
-        bytes_pp = len(source_region[0, 0])
-        target_region = combined_layer.get_pixel_rgn(0 ,0, source_width, source_height, True, True)
- 
-        source_pixels = array("B", source_region[0:source_width, 0:source_height])
-        target_pixels = array("B", "\x00" * (source_width * source_height * bytes_pp))
-
-        for x in range(0, source_width):
-            for y in range(0, source_height):
-                src_pos = (x + source_width * y) * bytes_pp
-                dest_pos = (x + source_width * y) * bytes_pp
-
-                target_pixels[dest_pos : dest_pos + bytes_pp] = source_pixels[src_pos: src_pos + bytes_pp]
-
-
-        # draw pixel regions                
-        target_region[0 : source_width, 0 : source_height] = target_pixels.tostring()
+       
+        for x in range(0,source_width):
+            for y in range(0,source_height):
+                cur_pixel = pdb.gimp_drawable_get_pixel(layer,x,y)
+                pixel_color = cur_pixel[1]
+                pixel_size = cur_pixel[0]
                 
+                if pixel_size == 3:
+                    pixel_size = 4
+                    new_pixel_color =[pixel_color[0], pixel_color[1], pixel_color[2], 255]
+                    pixel_color = new_pixel_color
+
+                pdb.gimp_drawable_set_pixel(combined_layer,combined_layer_offset_x + x, combined_layer_offset_y + y, pixel_size, pixel_color)
+
         # get offsets needed to fit in new layer
         if col_count == cols:
             col_count = 0
@@ -76,28 +69,30 @@ def Combine_Layers_To_Sheet():
 
     # display completed layer
     combined_layer.flush()
-    combined_layer.merge_shadow(True)
     combined_layer.update(0, 0, final_width, final_height)
-    gimp.gimp_image_set_resolution(img, final_height, final_width)
-    
+    img.resize(final_width, final_height, 0,0)
+    new_image = pdb.gimp_image_duplicate(img)   
+    combined_layer = pdb.gimp_image_merge_visible_layers(new_image, CLIP_TO_IMAGE)
+    pdb.gimp_file_save(new_image, combined_layer, 'F:/Development/Gimp/temp/test.png', '?')
+    pdb.gimp_image_delete(new_image)
 
 Combine_Layers_To_Sheet()
 
 
-gimpfu.register(
-    "Combine To Sprite Sheet",
-	"Combine all layers into a single sprite sheet - assumes all layers are the same size",
-	"Combine all layers into a single sprite sheet - assumes all layers are the same size",
-	"Brody Clark",
-	"Brody Clark",
-	"2022-2023",
-	"<Image>/Tools",
-	"RGB*, GRAY*",
-	[],
-	[],
-	Combine_Layers_To_Sheet,
-    menu = "<Image>/Tools"
-    )
+# gimpfu.register(
+#     "Combine To Sprite Sheet",
+# 	"Combine all layers into a single sprite sheet - assumes all layers are the same size",
+# 	"Combine all layers into a single sprite sheet - assumes all layers are the same size",
+# 	"Brody Clark",
+# 	"Brody Clark",
+# 	"2022-2023",
+# 	"<Image>/Tools",
+# 	"RGB*, GRAY*",
+# 	[],
+# 	[],
+# 	Combine_Layers_To_Sheet,
+#     menu = "<Image>/Tools"
+#     )
 
 
-gimpfu.main()
+# gimpfu.main()
